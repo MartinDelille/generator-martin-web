d6dba6baec0722a40a6b4fd776b1ddf607567c40
Martin Delille <martin@phonations.com>
2015-10-21 13:30:56 +0200

    gitignore

--- app/index.js
+++ app/index.js
@@ -4,6 +4,10 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
   writing: function() {
     this.fs.copyTpl(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore')
+        );
+    this.fs.copyTpl(
         this.templatePath('_package.json'),
         this.destinationPath('package.json'),
         { appname: slug(this.appname) }
